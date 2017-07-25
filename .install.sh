cc1d8aa4de5abf83ec8f81715f3adca4509d2fb2
Shinya Tsuda <shinya@dacci.org>
2017-07-26 00:06:50 +0900

    Updated install script.

--- .install.sh
+++ .install.sh
@@ -5,3 +5,7 @@ cd "$(dirname $0)"
 for f in *; do
   ln --symbolic --force --relative --no-target-directory "$f" "$HOME/.$f"
 done
+
+if [ -n "$APPDATA" ]; then
+  ln --symbolic --force --relative --target-directory="$HOME/AppData/Roaming" .roaming/*
+fi
